ec1efdb31f3123cbf62c4cbaf0b9d929362282a5
jpmon <jpmono416@gmail.com>
2017-09-18 15:57:11 +0100

    Trying to unify

--- WineDunk_CRUD_API/src/models/tblWines_.java
+++ WineDunk_CRUD_API/src/models/tblWines_.java
@@ -5,11 +5,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
 @Generated(value="Dali", date="2017-09-18T14:16:45.729+0100")
-=======
-@Generated(value="Dali", date="2017-09-17T11:31:56.243+0100")
->>>>>>> branch 'master' of https://github.com/jpmono416/Winedunk_CRUD_API.git
 @StaticMetamodel(tblWines.class)
 public class tblWines_ {
 	public static volatile SingularAttribute<tblWines, Integer> id;
