c12b181e0d37ab311f483bd64d7a4cd1736c297f
Jorge Barroso <jorge_barroso_11@hotmail.com>
2017-10-12 16:08:26 +0100

    Added methods to productfeeds

--- src/controllers/ProductFeeds.java
+++ src/controllers/ProductFeeds.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.io.StringWriter;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -22,6 +21,7 @@ import services.ProductFeedsService;
 @WebServlet("/ProductFeeds")
 public class ProductFeeds extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private final ObjectMapper mapper = new ObjectMapper();
 
 	@EJB
 	ProductFeedsService pfService;
@@ -29,27 +29,98 @@ public class ProductFeeds extends HttpServlet {
         super();
     }
 
-	/*protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-	}*/
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		if(!request.getParameterMap().containsKey("action"))
 			return;
 
 		switch(request.getParameter("action"))
 		{
-			case "getAll":				
-				final List<Tblpf> productFeeds = pfService.getAllProductFeeds();
-
-				final StringWriter outputString = new StringWriter();
-				final ObjectMapper objectMapper = new ObjectMapper();
-				objectMapper.writeValue(outputString, productFeeds);
-
-				response.setStatus(HttpServletResponse.SC_OK);
-				response.getWriter().write(outputString.toString());
-				break;				
+			case "getAll":
+			{
+				final List<Tblpf> productFeeds = this.pfService.getAllProductFeeds();
+				response.getWriter().write(this.mapper.writeValueAsString(productFeeds));
+				break;
+			}
+			case "getById":
+			{
+				if(request.getParameterMap().containsKey("id"))
+				{
+					response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing id");
+					return;
+				}
+				Tblpf pf = this.pfService.getById(Integer.parseInt(request.getParameter("id")));
+				if(pf!=null)
+					response.getWriter().print(this.mapper.writeValueAsString(pf));
+			}
+			case "failStandardisation":
+			{
+				if(request.getParameterMap().containsKey("id"))
+				{
+					response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing id");
+					return;
+				}
+				this.pfService.failStandardisation(Integer.parseInt(request.getParameter("id")));
+			}
+			case "okStandardisation":
+			{
+				if(request.getParameterMap().containsKey("id"))
+				{
+					response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing id");
+					return;
+				}
+				this.pfService.okStandardisation(Integer.parseInt(request.getParameter("id")));
+			}
+			case "processingStandardisation":
+			{
+				if(request.getParameterMap().containsKey("id"))
+				{
+					response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing id");
+					return;
+				}
+				this.pfService.processingStandardisation(Integer.parseInt(request.getParameter("id")));
+			}
+			case "failImportation":
+			{
+				if(request.getParameterMap().containsKey("id"))
+				{
+					response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing id");
+					return;
+				}
+				this.pfService.failStandardisation(Integer.parseInt(request.getParameter("id")));
+			}
+			case "okImportation":
+			{
+				if(request.getParameterMap().containsKey("id"))
+				{
+					response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing id");
+					return;
+				}
+				this.pfService.okStandardisation(Integer.parseInt(request.getParameter("id")));
+			}
+			case "processingImportation":
+			{
+				if(request.getParameterMap().containsKey("id"))
+				{
+					response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing id");
+					return;
+				}
+				this.pfService.processingStandardisation(Integer.parseInt(request.getParameter("id")));
+			}
 		}
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(!request.getParameterMap().containsKey("action"))
+			return;
+
+		//JsonNode json = this.mapper.readTree();
+		switch(request.getParameter("action"))
+		{
+			case "update":
+			{
+				Tblpf pf = this.mapper.readValue(request.getInputStream(), Tblpf.class);
+				response.getWriter().print(this.pfService.update(pf));
+			}
+		}
+	}
 }
--- src/services/ProductFeedsService.java
+++ src/services/ProductFeedsService.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import models.Tblpf;
+import models.Tblpfstatus;
 
 @Stateless
 @LocalBean
@@ -27,5 +28,83 @@ public class ProductFeedsService {
 			e.printStackTrace();
 			return null;
 		}
+	}
+
+	public Tblpf getById(Integer id)
+	{
+		try {
+			return em.find(Tblpf.class, id);
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public boolean failStandardisation(Integer id)
+	{
+		return this.setStandardisationStatus("Errors", id);
+	}
+
+	public boolean okStandardisation(Integer id)
+	{
+		return this.setStandardisationStatus("Ok", id);
+	}
+	
+	public boolean processingStandardisation(Integer id)
+	{
+		return this.setStandardisationStatus("Processing", id);
+	}
+
+	private boolean setStandardisationStatus(String statusName, Integer id)
+	{
+		try {
+			Tblpfstatus status = em.createNamedQuery("Tblpfstatus.findByName", Tblpfstatus.class).setParameter("name", statusName).getSingleResult();
+			em.find(Tblpf.class, id).setStandardisationStatus(status);
+			return true;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean failImportation(Integer id)
+	{
+		return this.setImportationStatus("Errors", id);
+	}
+
+	public boolean okImportation(Integer id)
+	{
+		return this.setImportationStatus("Ok", id);
+	}
+	
+	public boolean processingImportation(Integer id)
+	{
+		return this.setImportationStatus("Processing", id);
+	}
+
+	private boolean setImportationStatus(String statusName, Integer id)
+	{
+		try {
+			Tblpfstatus status = em.createNamedQuery("Tblpfstatus.findByName", Tblpfstatus.class).setParameter("name", statusName).getSingleResult();
+			em.find(Tblpf.class, id).setImportationStatus(status);
+			return true;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean update(Tblpf pf) {
+		if(pf.getId()==null || pf.getId()<1)
+			return false;
+
+		try {
+			em.merge(pf);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
 	}
 }
